5e9fc2d6cd497390f7bdf3de926d9ebfe2225742
Emi-Ureta <emilia.ureta.toro@gmail.com>
2022-09-25 18:31:05 -0300

    Spinners

--- app/src/main/java/ayuda/cl/geek_for_games/Activity_escribir_opinion.java
+++ app/src/main/java/ayuda/cl/geek_for_games/Activity_escribir_opinion.java
@@ -3,12 +3,51 @@ package ayuda.cl.geek_for_games;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class Activity_escribir_opinion extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Activity_escribir_opinion extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_escribir_opinion);
+
+        //elemento spinner
+        Spinner spinner_op = (Spinner) findViewById(R.id.spinner_opinion);
+
+        //Listener
+        spinner_op.setOnItemSelectedListener( this);
+
+        //Elementos para spinner
+        List<String> plataforma = new ArrayList<String>();
+        plataforma.add("PS5");
+        plataforma.add("Xbox X");
+        plataforma.add("PS4");
+        plataforma.add("Xbox 360");
+        plataforma.add("Nintendo Switch");
+        plataforma.add("PC");
+
+        //adaptador
+        ArrayAdapter<String> adaptador = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, plataforma);
+
+        adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner_op.setAdapter(adaptador);
+
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        // On selecting a spinner item
+        parent.getItemAtPosition(pos);
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
     }
 }
--- app/src/main/java/ayuda/cl/geek_for_games/Activity_lista_deseos.java
+++ app/src/main/java/ayuda/cl/geek_for_games/Activity_lista_deseos.java
@@ -3,12 +3,54 @@ package ayuda.cl.geek_for_games;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class Activity_lista_deseos extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Activity_lista_deseos extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_deseos);
+
+
+        //elemento spinner
+        Spinner spinner_lista_deseos = (Spinner) findViewById(R.id.spinner_deseos);
+
+        //Listener
+        spinner_lista_deseos.setOnItemSelectedListener(this);
+
+        //Elementos para spinner
+        List<String> plataforma_deseos = new ArrayList<String>();
+        plataforma_deseos.add("PS5");
+        plataforma_deseos.add("Xbox X");
+        plataforma_deseos.add("PS4");
+        plataforma_deseos.add("Xbox 360");
+        plataforma_deseos.add("Nintendo Switch");
+        plataforma_deseos.add("PC");
+
+        //adaptador
+        ArrayAdapter<String> adaptador_deseos = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, plataforma_deseos);
+
+        adaptador_deseos.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner_lista_deseos.setAdapter(adaptador_deseos);
+
     }
+
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        // On selecting a spinner item
+        parent.getItemAtPosition(pos);
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
+    }
+    }
+
 }
