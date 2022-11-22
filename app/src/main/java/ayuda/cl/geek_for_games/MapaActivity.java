4590eae9b9d5e50af0f826c63230353d1db166a7
Emi-Ureta <emilia.ureta.toro@gmail.com>
2022-11-22 14:00:39 -0300

    tiendas

--- app/src/main/java/ayuda/cl/geek_for_games/MapaActivity.java
+++ app/src/main/java/ayuda/cl/geek_for_games/MapaActivity.java
@@ -31,8 +31,34 @@ public class MapaActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
         LatLng chillan = new LatLng(-36.60787975863577, -72.10238905258322);
-        mMap.addMarker(new MarkerOptions().position(chillan).title("Chillan"));
+        mMap.addMarker(new MarkerOptions().position(chillan).title("Chillán"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(chillan));
+
+        LatLng microplay = new LatLng(-36.60961692116919, -72.10067318817647);
+        mMap.addMarker(new MarkerOptions().position(chillan).title("Microplay"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(microplay));
+
+
+        LatLng ripley = new LatLng(-36.60877701231661, -72.10166896092169);
+        mMap.addMarker(new MarkerOptions().position(chillan).title("Ripley"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ripley));
+
+        LatLng njoystick = new LatLng(-36.608411755746545, -72.1028380581252);
+        mMap.addMarker(new MarkerOptions().position(chillan).title("Njoystick"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(njoystick));
+
+
+        LatLng junix = new LatLng(-36.60901461842825, -72.10129310581279);
+        mMap.addMarker(new MarkerOptions().position(chillan).title("Junix Games"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(junix));
+
+
+        LatLng next = new LatLng(-36.60501841217301, -72.10558464001394);
+        mMap.addMarker(new MarkerOptions().position(chillan).title("Next Game Cyber"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(next));
+
+
+
 
     }
 }
